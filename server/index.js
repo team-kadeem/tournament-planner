50a6144b044996f5abd4b2c036e51135e15a3783
Kadeem Walsh <kadeemwalsh@Kadeems-MacBook-Pro.local>
2019-04-14 19:43:56 -0400

    express static middleware

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended:true }))
 app.use(bodyParser.json())
 app.use(bodyParser.text())
+app.use(express.static(path.join(__dirname, "../build")))
 
 
 /*
